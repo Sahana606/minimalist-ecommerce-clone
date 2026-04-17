7483041917ffe7f5a93aeb964e076bd9b0404255
Sahana606 <acharyasahana081@gmail.com>
2026-04-17 17:29:54 +0530

    Update Vite config for build output and server settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, 
+  }
 })
