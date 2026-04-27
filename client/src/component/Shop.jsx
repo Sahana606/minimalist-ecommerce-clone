8a24cfcf03fe580ce095ebd59d503d45cc9b4366
Sahana606 <acharyasahana081@gmail.com>
2026-04-27 11:16:06 +0530

    Refactor Shop component to fetch products correctly

--- client/src/component/Shop.jsx
+++ client/src/component/Shop.jsx
@@ -1,52 +1,53 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
 import "../style.css";
 
 function Shop() {
+  const [addproducts, setproducts] = useState([]);
+  const navigate = useNavigate();
 
-  const [addproducts,setproducts] = useState("");
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          "https://minimalist-ecommerce-clone.onrender.com/products"
+        );
 
-   useEffect(() => {
-      const getProducts = async () => {
-        try {
-  
-          const data = await axios.get(
-            "https://minimalist-ecommerce-clone.onrender.com/admin/products"
-          );
-  
-          setproducts(data);
-         
-  
-        } catch (err) {
-          console.log(err);
-        }
-      };
-  
-      getProducts();
-  
-    }, []);
+        setproducts(res.data); 
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getProducts();
+  }, []);
 
   return (
     <>
       <Header />
-       <div id="shopby"><h1>Collections</h1></div>
+      <div id="shopby"><h1>Collections</h1></div>
 
       <div className="c1">
-        {addproducts && addproducts?.data.map((product) => (
-          <div className="card2">
-          <img src={`minimalist-ecommerce-clone.onrender.com/uploads/${product.image}`} width="300" height="400" />
-          <h2><b>{product.name}</b></h2>
-           
-        </div>
+        {addproducts.map((product) => (
+          <div
+            key={product._id}
+            className="card2"
+            onClick={() => navigate(`/product/${product._id}`)} 
+            style={{ cursor: "pointer" }}
+          >
+            <img
+              src={`https://minimalist-ecommerce-clone.onrender.com/uploads/${product.image}`}
+              width="300"
+              height="400"
+            />
+            <h2><b>{product.name}</b></h2>
+          </div>
         ))}
-        
-
       </div>
-
-     
 
       <Footer />
     </>
