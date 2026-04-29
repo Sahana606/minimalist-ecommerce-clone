14ee5da32ec7b768b65465d72029d0a2168b0d64
Sahana606 <acharyasahana081@gmail.com>
2026-04-29 15:12:07 +0530

    Improve image handling in CartSidebar component
    Updated image source logic to handle different image formats and added a placeholder for missing images.

--- client/src/component/CartSidebar.jsx
+++ client/src/component/CartSidebar.jsx
@@ -83,7 +83,17 @@ function CartSidebar({ cart = [], setCart, showCart, setShowCart }) {
           cart.map(item => (
             <div className="cart-item" key={item.id}>
               <span className="remove-btn" onClick={() => removeItem(item.id)}>X</span>
-              <img src={item.images} width="60" alt={item.name} />
+             <img
+  src={
+    item.images?.[0]
+      ? item.images[0].startsWith("http")
+        ? item.images[0]
+        : `https://minimalist-ecommerce-clone.onrender.com/uploads/${item.images[0]}`
+      : "https://via.placeholder.com/60"
+  }
+  width="60"
+  alt={item.name}
+/>
               <div>
                 <h4>{item.name}</h4>
                 <p>{item.desc}</p>
