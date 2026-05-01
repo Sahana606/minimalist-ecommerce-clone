a09ac0d7460bbe7ff1375d139aa83d78850a604a
Sahana606 <acharyasahana081@gmail.com>
2026-05-01 14:09:59 +0530

    Comment out sendMail function
    Comment out the sendMail function to disable email sending.

--- server/index.js
+++ server/index.js
@@ -94,16 +94,16 @@ function generateOTP() {
   });
 }
 
-async function sendMail(to, subject, html) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html });
-}
+// async function sendMail(to, subject, html) {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+//   await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html });
+// }
 
 
 
