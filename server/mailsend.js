4404e9dd30460aaabfebfd04c0be390ca3eb8420
Sahana606 <acharyasahana081@gmail.com>
2026-05-01 16:42:21 +0530

    Enhance sendMail to include attachments
    Added support for attachments in sendMail function.

--- server/mailsend.js
+++ server/mailsend.js
@@ -16,13 +16,19 @@
 //     }
 // );
 
-async function sendMail(to, sub, msg) {
+async function sendMail(to, sub, msg,attachments =[]) {
   try {
     await transporter.sendMail({
       from: "proceess.env.EMAIL_FROM",
       to: to,
       subject: sub,
-      html: msg
+      html: msg,
+      attachments:attachments.map(file=>({
+        content:file.content.toString("base64"),
+        filename:file.filename,
+        type:"applications/pdf",
+        disposition:"attachment",
+      })),
     });
     console.log("Email sent successfully");
   } catch (error) {
